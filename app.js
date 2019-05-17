a2701c76e0294ee11b8702e706bebca695947c71
annejsize <annejsize@gmail.com>
2019-05-17 16:34:55 -0500

    click events

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
+
+//BUDGET CONTROLLER
 var budgetController = (function(){
-    var x = 23;
 
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
 })();
 
+// UI CONTROLLER
 var UIController = (function(){
 // some code
 })();
 
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-    
-    
-    var test = budgetCtrl.publicTest(4);
-    return {
-        anotherPublic: function(){
-            console.log(test);
-        }
+
+    var ctrlAddItem = function() {
+             //get the field input data
+        //add item to budget controller
+        //add item to UI controller
+        //calculate budget
+        //display budget
+        console.log("YO");
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keyup', function(event) {
+       if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem(); 
+       };
+    });
+
     })(budgetController, UIController);
