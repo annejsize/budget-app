20d86dd18ffa6baf92bde5d7b49d9563bf95a6ad
annejsize <annejsize@gmail.com>
2019-05-17 15:30:42 -0500

    create modules

---
+++ app.js
@@ -0,0 +1,28 @@
+var budgetController = (function(){
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b){
+            return add(b);
+        }
+    }
+})();
+
+var UIController = (function(){
+// some code
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+    
+    
+    var test = budgetCtrl.publicTest(4);
+    return {
+        anotherPublic: function(){
+            console.log(test);
+        }
+    }
+    })(budgetController, UIController);
